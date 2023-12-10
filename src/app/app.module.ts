fc460bcfbab8768816ac881fbc9ae9682e0617b4
Alexandre RAVICHANDRAN <alexandre.ravichandran@gmail.com>
2023-12-10 11:33:16 +0100

    feat(auth): Create token interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthInterceptor } from './shared/interceptor/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,12 @@ import { AppComponent } from './app.component';
     HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor,
+      multi: true
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/interceptor/auth.interceptor.spec.ts
@@ -0,0 +1,53 @@
+import { TestBed } from '@angular/core/testing';
+
+import { AuthInterceptor } from './auth.interceptor';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
+
+describe('Testing Auth interceptor', () => {
+
+  let httpMock: HttpTestingController;
+  let interceptor: AuthInterceptor;
+  let httpClient: HttpClient;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: AuthInterceptor,
+          multi: true
+        },
+        AuthInterceptor
+      ]
+    });
+
+    interceptor = TestBed.inject(AuthInterceptor);
+    httpMock = TestBed.inject(HttpTestingController);
+    httpClient = TestBed.inject(HttpClient);
+
+  });
+
+  afterEach(() => {
+
+    httpMock.verify();
+
+  })
+
+  it('should be created', () => {
+    const interceptor: AuthInterceptor = TestBed.inject(AuthInterceptor);
+    expect(interceptor).toBeTruthy();
+  });
+
+  it('should add token in request when token exists', () => {
+    sessionStorage.setItem('token', 'testtoken');
+    httpClient.get('test').subscribe();
+
+    const request = httpMock.expectOne('test');
+
+    expect(request.request.headers.get('Authorization')).toEqual('Bearer testtoken');
+
+  });
+
+});
