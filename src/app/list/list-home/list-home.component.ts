13da5adc4fe29d606712408e93243fc889fb6f20
Alexandre RAVICHANDRAN <alexandre.ravichandran@gmail.com>
2023-12-20 09:35:58 +0100

    feat(user): Create main user list page

---
+++ src/app/list/list-home/list-home.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-home',
+  templateUrl: './list-home.component.html',
+  styleUrls: ['./list-home.component.scss']
+})
+export class ListHomeComponent {
+
+  constructor() { }
+}
