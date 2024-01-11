f83cdefc1bd56fdc6fbdd486113f49a4a453d6d4
Alexandre RAVICHANDRAN <alexandre.ravichandran@gmail.com>
2024-01-11 20:03:42 +0100

    fix(all): fix scrollbar displaying issue and add missing modules

--- src/app/list/list.module.ts
+++ src/app/list/list.module.ts
@@ -4,11 +4,13 @@ import { ListHomeComponent } from './list-home/list-home.component';
 import { MovieListItemComponent } from './movie-list/movie-list-item/movie-list-item.component';
 import { MovieListHomeComponent } from './movie-list/movie-list-home/movie-list-home.component';
 import { CommonModule } from "@angular/common";
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { SharedModule } from "../shared/shared.module";
 import { MusicListHomeComponent } from './music-list/music-list-home/music-list-home.component';
 import { MusicListItemComponent } from './music-list/music-list-item/music-list-item.component';
 import { MovieListShowComponent } from './movie-list/movie-list-show/movie-list-show.component';
 import { MusicListShowComponent } from './music-list/music-list-show/music-list-show.component';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,9 @@ import { MusicListShowComponent } from './music-list/music-list-show/music-list-
   imports: [
     ListRoutingModule,
     CommonModule,
-    SharedModule
+    SharedModule,
+    DragDropModule,
+    MatTooltipModule
   ],
   providers: []
 })
