da511bf41969fad19507a10239c50f1a417dae75
Alexandre RAVICHANDRAN <alexandre.ravichandran@gmail.com>
2024-01-14 15:48:28 +0100

    feat(login): Add ngrx elements to register page

--- src/app/home/register/register.component.ts
+++ src/app/home/register/register.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { ApiError } from 'src/app/shared/error/api-error';
-import { AuthResponse } from 'src/app/shared/models/auth/auth-response';
 import { AuthService } from 'src/app/shared/services/auth/auth.service';
 import { MusicService } from 'src/app/shared/services/music/music.service';
+import { AppUserState } from '../state/app-user.state';
+import { getError, getIsLoading } from '../state/selectors/app-user.selectors';
+import { AppUserPageActions } from '../state/actions';
+import { AppUser } from 'src/app/shared/models/appuser/appuser';
 
 @Component({
   selector: 'mlm-register',
@@ -15,15 +18,15 @@ import { MusicService } from 'src/app/shared/services/music/music.service';
 export class RegisterComponent {
 
   constructor(
+    private appUserStore: Store<AppUserState>,
     private authService: AuthService,
-    private router: Router,
     private musicService: MusicService
   ) { }
 
   registerForm: FormGroup = this.authService.generateRegisterForm();
   randomMusicPictureUrl$: Observable<string> = this.musicService.getRandomIllustrationPictureUrl();
-  apiError: ApiError | null = null;
-  isLoading: boolean = false;
+  apiError: Observable<ApiError | null> = this.appUserStore.select(getError);
+  isLoading: Observable<boolean> = this.appUserStore.select(getIsLoading);
 
   isFormValid(): boolean {
     return this.registerForm.valid;
@@ -35,32 +38,12 @@ export class RegisterComponent {
       return;
     }
 
-    this.isLoading = true;
+    const appUser: AppUser = {
+      username: this.registerForm.get('username')?.value,
+      password: this.registerForm.get('password')?.value,
+    }
 
-    this.authService.register(this.registerForm.value).subscribe({
-      next: (token: AuthResponse) => {
-        this.handleAuthSuccessful(token);
-      },
-      error: (error: ApiError) => {
-        this.handleAuthFailure(error);
-      }
-    });
+    this.appUserStore.dispatch(AppUserPageActions.toggleLoading());
+    this.appUserStore.dispatch(AppUserPageActions.register({ appUser }));
   }
-
-  private handleAuthSuccessful(authResponse: AuthResponse): void {
-
-    sessionStorage.setItem('token', authResponse.token);
-    this.isLoading = false;
-    this.router.navigate(['me']);
-
-  }
-
-  private handleAuthFailure(error: ApiError): void {
-
-    this.apiError = error;
-    sessionStorage.removeItem('token');
-    this.isLoading = false;
-
-  }
-
 }
