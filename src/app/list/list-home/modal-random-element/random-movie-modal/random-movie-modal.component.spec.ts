159eb64c4d71fbb379ad85dcecc8e7a6229f8cf6
Alexandre RAVICHANDRAN <alexandre.ravichandran@gmail.com>
2024-02-15 20:16:57 +0100

    fix(movie/music): Add test for random generation of movie/music

--- src/app/list/list-home/modal-random-element/random-movie-modal/random-movie-modal.component.spec.ts
+++ src/app/list/list-home/modal-random-element/random-movie-modal/random-movie-modal.component.spec.ts
@@ -2,26 +2,80 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RandomMovieModalComponent } from './random-movie-modal.component';
 import { ListModule } from 'src/app/list/list.module';
+import { MovieService } from 'src/app/shared/services/movie/movie.service';
+import { MovieListService } from 'src/app/shared/services/list/movie/movie-list.service';
+import { Movie } from 'src/app/shared/models/movie/movie';
+import { of } from 'rxjs';
+import { MovieListItem } from 'src/app/shared/models/list/movie/movie-list-item';
+import { AppModule } from 'src/app/app.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('Testing random movie modal component', () => {
   let component: RandomMovieModalComponent;
   let fixture: ComponentFixture<RandomMovieModalComponent>;
+  let mockMovieService: jasmine.SpyObj<MovieService>;
+  let mockMovieListService: jasmine.SpyObj<MovieListService>;
+  let movieListMock: any;
+  let movieMock: any;
 
   beforeEach(async () => {
+
+    mockMovieService = jasmine.createSpyObj('MovieService', ['readById']);
+    mockMovieListService = jasmine.createSpyObj('MovieListService', ['getRandom']);
+
     await TestBed.configureTestingModule({
       declarations: [RandomMovieModalComponent],
-      imports: [ListModule]
+      imports: [ListModule, BrowserAnimationsModule],
+      providers: [
+        { provide: MovieService, useValue: mockMovieService },
+        { provide: MovieListService, useValue: mockMovieListService }
+      ],
     })
       .compileComponents();
+
+    const movieList: MovieListItem = {
+      id: 1,
+      movieId: 2,
+      addedAt: new Date(),
+      appUserId: 1,
+      sortingOrder: 1,
+      movieDetail: undefined
+    }
+
+    const movie: Movie = {
+      id: 1,
+      apiCode: '001',
+      pictureUrl: 'http://www.google.com',
+      releasedAt: 2011,
+      title: 'Movie 1'
+    }
+
+    movieListMock = mockMovieListService.getRandom.and.returnValue(of(movieList));
+
+    movieMock = mockMovieService.readById.and.returnValue(of(movie));
+
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(RandomMovieModalComponent);
     component = fixture.componentInstance;
+
+
     fixture.detectChanges();
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should get random movie when click on button', () => {
+
+    component.generateNewRandomElement();
+
+    expect(movieListMock).toHaveBeenCalled();
+    expect(movieMock).toHaveBeenCalled();
+
+  });
+
 });
--- src/app/list/list-home/modal-random-element/random-music-modal/random-music-modal.component.spec.ts
+++ src/app/list/list-home/modal-random-element/random-music-modal/random-music-modal.component.spec.ts
@@ -2,17 +2,60 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RandomMusicModalComponent } from './random-music-modal.component';
 import { ListModule } from 'src/app/list/list.module';
+import { MusicService } from 'src/app/shared/services/music/music.service';
+import { MusicListService } from 'src/app/shared/services/list/music/music-list.service';
+import { MusicListItem } from 'src/app/shared/models/list/music/music-list-item';
+import { Music } from 'src/app/shared/models/music/music';
+import { of } from 'rxjs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('Testing random music modal component', () => {
   let component: RandomMusicModalComponent;
   let fixture: ComponentFixture<RandomMusicModalComponent>;
+  let mockMusicService: jasmine.SpyObj<MusicService>;
+  let mockMusicListService: jasmine.SpyObj<MusicListService>;
+  let musicListMock: any;
+  let musicMock: any;
 
   beforeEach(async () => {
+
+    mockMusicService = jasmine.createSpyObj('MusicService', ['readById']);
+    mockMusicListService = jasmine.createSpyObj('MusicListService', ['getRandom']);
+
+
     await TestBed.configureTestingModule({
       declarations: [RandomMusicModalComponent],
-      imports: [ListModule]
+      imports: [ListModule, BrowserAnimationsModule],
+      providers: [
+        { provide: MusicService, useValue: mockMusicService },
+        { provide: MusicListService, useValue: mockMusicListService }
+      ],
     })
       .compileComponents();
+
+    const musicList: MusicListItem = {
+      id: 1,
+      musicId: 2,
+      addedAt: new Date(),
+      appUserId: 1,
+      sortingOrder: 1,
+      musicDetail: undefined
+    }
+
+    const music: Music = {
+      id: 1,
+      apiCode: '001',
+      pictureUrl: 'http://www.google.com',
+      releasedAt: 2011,
+      title: 'Music 1',
+      artistName: 'Artist',
+      type: 2
+    }
+
+    musicListMock = mockMusicListService.getRandom.and.returnValue(of(musicList));
+
+    musicMock = mockMusicService.readById.and.returnValue(of(music));
+
   });
 
   beforeEach(() => {
@@ -24,4 +67,16 @@ describe('Testing random music modal component', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should get random music when click on button', () => {
+
+    component.generateNewRandomElement();
+
+    expect(musicListMock).toHaveBeenCalled();
+    expect(musicMock).toHaveBeenCalled();
+
+  });
+
+
+
 });
